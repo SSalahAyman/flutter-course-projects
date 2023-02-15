d907951b99bf1e875b08978f1dff4fff0c989640
Salah Ayman <salahayman488@gmail.com>
2023-02-15 23:57:25 +0200

    fixed media query problem

--- languagelearningapp/lib/components/category_item.dart
+++ languagelearningapp/lib/components/category_item.dart
@@ -4,35 +4,33 @@ class Category extends StatelessWidget {
   String? text;
   Color? color;
   VoidCallback? ontap;
-  // late double width;
-  // late double height;
+  late double width;
+  late double height;
 
   Category({this.text, this.color, this.ontap});
 
   @override
   Widget build(BuildContext context) {
-    // width = MediaQuery.of(context).size.width;
-    // height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          ontap;
-        },
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height * 0.09,
-          color: color,
-          child: Text(
-            text!,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-            ),
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+    return GestureDetector(
+      onTap: () {
+        ontap;
+      },
+      child: Container(
+        width: width,
+        height: height * 0.1,
+        color: color,
+        child: Text(
+          text!,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
           ),
-          alignment: Alignment.centerLeft,
-          padding:
-              EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.04),
         ),
+        alignment: Alignment.centerLeft,
+        padding:
+            EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.04),
       ),
     );
   }
--- languagelearningapp/lib/screens/home_page.dart
+++ languagelearningapp/lib/screens/home_page.dart
@@ -9,33 +9,37 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Mywidth = MediaQuery.of(context).size.width;
-    // Myheight = MediaQuery.of(context).size.height;
+    Mywidth = MediaQuery.of(context).size.width;
+    Myheight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: Text("Toku"),
         backgroundColor: Color(0xff46322B),
       ),
       body: Container(
-        // width: Mywidth,
-        // height: Myheight,
+        width: Mywidth,
+        height: Myheight,
         child: Column(
           children: [
-            // Category(
-            //   text: "numbers",
-            //   color: Color(0xfff99430),
-            //   ontap: () {},
-            // ),
-            // Category(
-            //   text: 'Family Members',
-            //   color: Color(0xff517f31),
-            //   ontap: () {},
-            // ),
-            // Category(
-            //   text: 'Colors',
-            //   color: Color(0xff7c3fa1),
-            //   ontap: () {},
-            // ),
+            Category(
+              text: "numbers",
+              color: Color(0xfff99430),
+              ontap: () {
+                print("number category");
+              },
+            ),
+            Category(
+              text: 'Family Members',
+              color: Color(0xff517f31),
+              ontap: () {
+                print("family members category");
+              },
+            ),
+            Category(
+              text: 'Colors',
+              color: Color(0xff7c3fa1),
+              ontap: () {},
+            ),
             Category(
               text: 'Phrases',
               color: Color(0xff46a4ca),
