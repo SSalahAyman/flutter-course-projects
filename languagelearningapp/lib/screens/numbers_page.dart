448aeed6bc690535d9a354fe75a3680351b52c19
Salah Ayman <salahayman488@gmail.com>
2023-02-21 02:59:00 +0200

    add  listview.builder

--- languagelearningapp/lib/screens/numbers_page.dart
+++ languagelearningapp/lib/screens/numbers_page.dart
@@ -6,10 +6,56 @@ class Numberspage extends StatelessWidget {
   const Numberspage({super.key});
   static late double width;
   static late double height;
-  final Number one = const Number(
-      image: "assets/images/numbers/number_one.png",
-      enName: "one",
-      jpName: "ichi");
+  // final Number one = const Number(
+  //     image: "assets/images/numbers/number_one.png",
+  //     enName: "one",
+  //     jpName: "ichi");
+
+  /*
+      creating list of numbers and the values of this list type of Number class
+     */
+  final List<Number> numbers = const [
+    Number(
+        image: "assets/images/numbers/number_one.png",
+        enName: "one",
+        jpName: "ichi"),
+    Number(
+        image: "assets/images/numbers/number_two.png",
+        enName: "two",
+        jpName: "ichi"),
+    Number(
+        image: "assets/images/numbers/number_three.png",
+        enName: "three",
+        jpName: "ichi"),
+    Number(
+        image: "assets/images/numbers/number_four.png",
+        enName: "four",
+        jpName: "ichi"),
+    Number(
+        image: "assets/images/numbers/number_five.png",
+        enName: "five",
+        jpName: "ichi"),
+    Number(
+        image: "assets/images/numbers/number_six.png",
+        enName: "six",
+        jpName: "ichi"),
+    Number(
+        image: "assets/images/numbers/number_seven.png",
+        enName: "seven",
+        jpName: "ichi"),
+    Number(
+        image: "assets/images/numbers/number_eight.png",
+        enName: "eight",
+        jpName: "ichi"),
+    Number(
+        image: "assets/images/numbers/number_nine.png",
+        enName: "nine",
+        jpName: "ichi"),
+    Number(
+        image: "assets/images/numbers/number_ten.png",
+        enName: "ten",
+        jpName: "ichi"),
+  ];
   @override
   Widget build(BuildContext context) {
     width = MediaQuery.of(context).size.width;
@@ -22,11 +68,40 @@ class Numberspage extends StatelessWidget {
       body: Container(
         height: height,
         width: width,
-        child: Column(children: [
-          Item(number: one),
-          Item(number: one),
-        ]),
+        child: ListView.builder(
+          itemCount: numbers.length,
+          itemBuilder: (context, index) {
+            print(index);
+            return Item(
+              number: numbers[index],
+            );
+          },
+
+          // Item(number: numbers[0]),
+          // Item(number: numbers[1]),
+          // Item(number: numbers[2]),
+          // Item(number: numbers[3]),
+          // Item(number: numbers[4]),
+          // Item(number: numbers[5]),
+          // Item(number: numbers[6]),
+          // Item(number: numbers[7]),
+          // Item(number: numbers[8]),
+          // Item(number: numbers[9]),
+          /*********************************************************************************************************/
+
+          // getlist(numbers),
+
+          /*********************************************************************************************************/
+        ),
       ),
     );
+  }
+
+  List<Widget> getlist(List<Number> numbers) {
+    List<Widget> itemslist = [];
+    for (int i = 0; i < numbers.length; i++) {
+      itemslist.add(Item(number: numbers[i]));
+    }
+    return itemslist;
   }
 }
