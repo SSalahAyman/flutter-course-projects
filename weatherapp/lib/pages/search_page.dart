c1827319befd9c15b1a75c1ea002c58edac4e450
Salah Ayman <59060196+SSalahAyman@users.noreply.github.com>
2023-05-12 23:54:04 +0300

    finished the appbar of the search page

--- weatherapp/lib/pages/search_page.dart
+++ weatherapp/lib/pages/search_page.dart
@@ -2,9 +2,25 @@ import 'package:flutter/material.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
-
+  static late double width;
+  static late double height;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(
+            "Search a city",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child: TextField(),
+        ),
+      ),
+    );
   }
 }
